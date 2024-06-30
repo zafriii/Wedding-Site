6a62875adf6ad3ebb5e0750cd4a87683209405bc
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-07-01 03:30:45 +0600

    Add files via upload

---
+++ src/components/Pronav.jsx
@@ -0,0 +1,43 @@
+// import React from 'react'
+// import { NavLink } from 'react-router-dom';
+// import './profile.css'
+
+// function Pronav() {
+//   return (
+//     <div className='pronav'>
+//         <div className='pronav-links'>
+//                     <NavLink to="/personal_info" className='active' >Personal Informations</NavLink>
+//                     <NavLink to="/notification">Notifications</NavLink>
+//                     <NavLink to="/search" >Search Profiles</NavLink>
+//         </div>
+
+//     </div>
+//   )
+// }
+
+// export default Pronav
+
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './profile.css';
+
+function Pronav() {
+  return (
+    <div className='pronav'>
+      <div className='pronav-links'>
+        <NavLink to="/personal_info" className={({ isActive }) => (isActive ? 'active' : '')}>
+          Personal Information
+        </NavLink>
+        <NavLink to="/notification" className={({ isActive }) => (isActive ? 'active' : '')}>
+          Notifications
+        </NavLink>
+        <NavLink to="/search" className={({ isActive }) => (isActive ? 'active' : '')}>
+          Search Profiles
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default Pronav;
