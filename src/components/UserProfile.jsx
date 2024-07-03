7f8478b90188ad3e52ca85c7a51eaa7ab525d05f
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-07-04 01:04:51 +0600

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,70 +1,7 @@
-// import React, { useState, useEffect } from 'react';
-// import './userprofile.css';
-// import Pronav from './Pronav';
-
-// function UserProfile() {
-//     const [username, setUsername] = useState('');
-
-//     useEffect(() => {
-//       const fetchUsername = async () => {
-//         try {
-//           const response = await fetch('/api/user/', { //Your API Endpoint
-//             headers: {
-//               'Content-Type': 'application/json',
-//               // Add any authentication headers if needed
-//               // e.g., Authorization: `Bearer ${accessToken}`
-//             }
-//           });
-//           if (!response.ok) {
-//             throw new Error('Failed to fetch username');
-//           }
-//           const data = await response.json();
-//           setUsername(data.username);
-//         } catch (error) {
-//           console.error('Error fetching username:', error);
-//         }
-//       };
-  
-//       fetchUsername();
-//     }, []);
-  
-//     return (
-//       <div className='profile'>
-//         <div className="welcome">
-//           {username ? (
-//             <h2>Welcome, {username}</h2>
-//           ) : (
-//             <p>Loading...</p>
-//           )}
-//         </div>
-//         <Pronav/>
-      
-
-//           <div className="user-profile">
-//             <div className="profile-item">
-//             <h2>User profile</h2>
-//             <div className="profile-content">
-               
-//             </div>
-//             </div>
-//           </div>
-
-
-//       </div>
-//     );
-// }
-
-// export default UserProfile
-
-
-
-
 import React, { useState, useEffect,useContext } from 'react';
 import './userprofile.css';
 import Pronav from './Pronav';
 import { ProfileContext } from './ProfileContext';
-
-
 
 function UserProfile() {
     const [username, setUsername] = useState('');
