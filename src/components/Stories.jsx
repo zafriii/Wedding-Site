73ac0e48bd4592f4bbb6d965aaad8f85c52dee2b
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-07-04 23:49:43 +0600

    Add files via upload

---
+++ src/components/Stories.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './stories.css'
+
+function Stories() {
+  return (
+    <div className='stories'>
+
+        <h2>Success Stories</h2>
+        <div className='stories-container'>
+
+         <div className="story-card">
+
+            <img src='images/paralax.jpg'></img>
+
+            <p>Our stories</p>
+
+         </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Stories
